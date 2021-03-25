8b2c92ca64a1fbeb7387a2439759d92172926cc8
Adrian Lawson <adrianboblawson@csu.fullerton.edu>
2021-03-25 12:53:28 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ Moralis.initialize("");
 Moralis.serverURL = ''
 
 init = async () => {
+    hideElement(userInfo);
     window.web3 = await Moralis.Web3.enable();
     initUser();
 }
@@ -16,7 +17,7 @@ initUser = async () => {
     }
 }
 
-login = async() => {
+login = async () => {
     try {
         await Moralis.Web3.authenticate();
         initUser();
@@ -25,12 +26,75 @@ login = async() => {
     }
 }
 
+logout = async () => {
+    await Moralis.User.logOut();
+    hideElement(userInfo);
+    initUser();
+}
+
+openUserInfo = async () => {
+    user = await Moralis.User.current();
+    if(user){
+        const email = user.get('email');
+        if(email){
+            userEmailField.value = email;
+        }else {
+            userEmailField.value = "";
+        }
+
+        userUsernameField.value = user.get('username');
+
+        const userAvatar = user.get('avatar');
+        if(userAvatar) {
+            userAvatarImg.src = userAvatar.url();
+            showElement(userAvatarImg);
+        }else {
+            hideElement(userAvatarImg);
+
+        }
+
+        showElement(userInfo);
+    }else {
+        login();
+    }
+}
+
+saveUserInfo = async () => {
+    user.set('email', userEmailField.value);
+    user.set('username', userUsernameField.value);
+
+    if (userAvatarFile.files.length > 0) {
+      
+        const avatar = new Moralis.File("avatar.jpg", userAvatarFile.files[0]);
+        user.set('avatar', avatar);
+    }
+
+    await user.save();
+    alert("User info saved successfully");
+    openUserInfo();
+}
+
 hideElement = (element) => element.style.display = "none";
 showElement = (element) => element.style.display = "block";
 
 const userConnectButton = document.getElementById("btnConnect");
 userConnectButton.onclick = login;
 const userProfileButton = document.getElementById("btnUserInfo");
+userProfileButton.onclick = openUserInfo;
+
+const userInfo = document.getElementById("userInfo");
+const userClose = document.getElementById("btnCloseUserInfo");
+userClose.onclick = () => hideElement(userInfo);
+const userLogout = document.getElementById("btnLogout");
+userLogout.onclick = logout;
+const userUsernameField = document.getElementById("txtUsername");
+const userEmailField = document.getElementById("txtEmail");
+const userAvatarImg = document.getElementById("imgAvatar");
+const userAvatarFile = document.getElementById("fileAvatar");
+
+const userSaveButton = document.getElementById("btnSaveUserInfo");
+userSaveButton.onclick = saveUserInfo;
+
 
 
 init();
