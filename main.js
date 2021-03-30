125e11a4a695777425a09e93a5d487d7060131f8
Adrian Lawson <adrianboblawson@csu.fullerton.edu>
2021-03-30 15:39:30 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 Moralis.initialize("AdEIwBVQF3IB6TpjoyZidRasNgLuyGAzdfJJdLOT");
-Moralis.serverURL = 'https://oo3xsmnesgip.moralis.io:2053/server'
-const TOKEN_CONTRACT_ADDRESS = "0xA3384210527705238b63493309B894F2cB863835";
+Moralis.serverURL = 'https://uavkeiigq1dw.moralis.io:2053/server'
+const TOKEN_CONTRACT_ADDRESS = "65Sp0XaKbuzcDeRBtpRCUkMfQ3ICogBxjbKIWiWP";
 
 init = async () => {
     hideElement(createItemForm);
