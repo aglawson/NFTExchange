aca9bfc5f6cbd6476d09b5c0f4fd42b1e39c1a3a
Adrian Lawson <adrianboblawson@csu.fullerton.edu>
2021-04-08 12:22:57 -0700

    Added auction smart contract, not functional yet

---
+++ contracts/Auction.sol
@@ -0,0 +1,181 @@
+pragma solidity 0.7.2;
+
+import "./Marketplace.sol";
+import "./MoraribleToken.sol";
+
+contract Auction {
+
+  /* Determine the state of the auction. */
+  enum AuctionState { IN_PROGRESS, CALCULATING, COMPLETED }
+
+  /* EVENTS */
+  event Bid(address bidder);
+  event Winner(address winner, uint bidValue);
+
+  /* BIDDER */
+  struct Bidder {
+    bool hasBidded;
+    bytes bidValue;
+  }
+
+  /* STATE VARIABLES */
+  address public owner;
+  uint public startTime;
+  uint public endTime;
+  address public winner;
+  uint public startingPrice;
+  uint public winningPrice;
+  mapping(address => Bidder) public bidders;
+  mapping(address => uint) public stakeAmounts;
+  address[] public bidderAddresses;
+  MoraribleToken public mora;
+  AuctionState public state;
+  bool public rewardClaimed;
+
+  /* CONSTRUCTOR */
+  constructor(address _owner, uint _auctionLength, uint _startingPrice, address _moraCollectible) public {
+    owner = _owner;
+    startingPrice = _startingPrice;
+    startTime = block.timestamp;
+    endTime = startTime + _auctionLength * 1 seconds;
+    mora = MoraribleToken(_moraCollectible);
+    state = AuctionState.IN_PROGRESS;  // redundant
+  }
+
+  /*
+   * Stake Ether in the contract to create a binding commitment. You can increase stake anytime within the bidding period.
+   */
+  function stake() payable external {
+    require(state == AuctionState.IN_PROGRESS);
+    stakeAmounts[msg.sender] += msg.value;
+  }
+
+  /*
+   * Withdraw Ether after the bidding period has ended.
+   */
+  function withdraw() external {
+    require(state == AuctionState.COMPLETED);
+    require(stakeAmounts[msg.sender] > 0);
+    uint amount = stakeAmounts[msg.sender];
+    stakeAmounts[msg.sender] = 0;
+    msg.sender.transfer(amount);
+  }
+
+  /*
+   * Bid in the auction. The value of the bid(in wei) is encrypted.
+   * NOTE: A user can bid multiple times as long as it's within the bidding period.
+   */
+  function bid(bytes memory _bidValue) external {
+    require(block.timestamp < endTime);  // a user can only bid within the time period
+    require(stakeAmounts[msg.sender] >= startingPrice);  // a user can only bid if they have enough stake to fill the initial starting value
+    bidders[msg.sender].bidValue = _bidValue;
+    if (!(bidders[msg.sender].hasBidded)) {
+      bidders[msg.sender].hasBidded = true;
+    }
+    emit Bid(msg.sender);
+  }
+
+  /*
+   * End the auction. Only the creator of the auction can end the auction(when the bidding period has expired).
+   */
+  function endAuction() external isOwner {
+    require(state == AuctionState.IN_PROGRESS);
+    //require(now >= endTime);
+    state = AuctionState.CALCULATING;
+  }
+
+  /*
+   * The callable function. Gets the highest bidder and bid amount for the auction.
+   * NOTE: In the event of a tie, we're just taking the first bidder.
+   */
+  function getHighestBidder(address[] memory _bidders, uint[] memory _bidAmounts, uint[] memory _stakeAmounts) public pure returns (address, uint) {
+    address highestBidder;
+    uint highestBidAmount;
+    for (uint i = 0; i < _bidders.length; i++) {
+      if ((_bidAmounts[i] > highestBidAmount) && (_bidAmounts[i] <= _stakeAmounts[i])) {
+        highestBidAmount = _bidAmounts[i];
+        highestBidder = _bidders[i];
+      }
+    }
+    return (highestBidder, highestBidAmount);
+  }
+
+  /*
+   * The callback function. Updates the contract state.
+   */
+  function updateWinner(address _highestBidder, uint _highestBidAmount) public
+    //onlyEnigma comment this out for testing
+    {
+    winner = _highestBidder;
+    winningPrice = _highestBidAmount;
+    state = AuctionState.COMPLETED;
+    stakeAmounts[_highestBidder] -= winningPrice;
+    emit Winner(_highestBidder, _highestBidAmount);
+  }
+
+  /*
+   * Allow a user to claim their reward.
+   */
+  function claimReward() external {
+    require(state == AuctionState.COMPLETED);
+    require(msg.sender == winner);
+    require(!rewardClaimed);
+    rewardClaimed = true;
+    mora.createItem(msg.sender, endTime);  // mint an ERC721 Enigma Collectible with arbitrary tokenID (just use the end time)
+  }
+
+  /*
+   * Allow the creator of the auction to claim the winner's stake.
+   */
+  function claimEther() external isOwner {
+    require(state == AuctionState.COMPLETED);
+    msg.sender.transfer(winningPrice);
+  }
+
+  /*
+   * Modifier that checks that the contract caller is the Enigma contract.
+   */
+  modifier onlyEnigma() {
+    require(msg.sender == address(mora));
+    _;
+  }
+
+  /*
+   * Modifier that checks if the caller is the creator of the auction.
+   */
+  modifier isOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  /*
+   * Check that a user has bidded.
+   */
+  function hasBidded(address _bidder) public view returns (bool) {
+    return bidders[_bidder].hasBidded;
+  }
+
+  /*
+   * Get the encrypted bid value for a bidder.
+   */
+  function getBidValueForBidder(address _bidder) public view returns (bytes memory) {
+    require(hasBidded(_bidder), "User has not bidded yet.");
+    return bidders[_bidder].bidValue;
+  }
+
+  /*
+   * Get the staked Ether amount for a given bidder.
+   */
+  function getStakeOfBidder(address _bidder) public view returns (uint) {
+    return stakeAmounts[_bidder];
+  }
+
+  /*
+   * Get the winner of the auction (if available).
+   */
+  function getWinner() public view returns(address) {
+    require(state == AuctionState.COMPLETED);
+    return winner;
+  }
+
+}
