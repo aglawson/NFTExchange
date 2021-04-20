ca04775765ff74981d908c9617cff8cbfe5129a2
Adrian Lawson <adrianboblawson@csu.fullerton.edu>
2021-04-19 17:15:03 -0700

    Update main.js

--- main.js
+++ main.js
@@ -257,7 +257,7 @@ renderUserItem = async (item) => {
     userItems.appendChild(userItem);
 
 
-    const myObject = await Moralis.Cloud.run('getItem', item.attributes.id);
+    const myObject = await Moralis.Cloud.run('getItem', item.id);
     userItem.getElementsByTagName("button")[1].onclick = () => {
            console.log(myObject);
 //         userItem.parentNode.removeChild(userItem);
